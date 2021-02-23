258bb9be9706d3e83100da3fd7f2aa6a3202906e
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-23 20:04:23 +0530

    [MaheshKangude] ADDED: ABilityToCreateC#PogramToConnectDataBase

---
+++ EmployeePayrollService/EmployeeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    class EmployeeModel
+    {
+        public int EmpID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public double PhoneNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Department { get; set; }
+        public double BasicPay { get; set; }
+        public double Deduction { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public double NetPay { get; set; }
+    }
+}
--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace EmployeePayrollService
 {
     class Program
     {
+        static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeePayroll;User Id=mahesh;Password=root";
+
+        public static DataSet GetAllEmployeeData()
+        {
+            EmployeeModel employee = new EmployeeModel();
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                using (connection)
+                {                   
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return null;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Employee Payroll Service");
+            GetAllEmployeeData();
         }
     }
 }
